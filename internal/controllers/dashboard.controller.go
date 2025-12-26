733a9fdeb90028de25683f59deafa0e6f72c1b85
Derek Garnett <drkgrntt@gmail.com>
2025-12-25 23:29:57 -0600

    Add rating calendar to dashboard

--- internal/controllers/dashboard.controller.go
+++ internal/controllers/dashboard.controller.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"go-starter/cmd/web/dashboard"
 	"go-starter/internal/middleware"
+	"go-starter/internal/models"
 	"go-starter/internal/utils"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -25,9 +27,26 @@ func (c *DashboardController) Init(db *gorm.DB, app *fiber.App) {
 	c.api = app.Group("api/dashboard")
 }
 
+func (c *DashboardController) getJournals(ctx *fiber.Ctx) error {
+	currentUser := utils.GetLocal[models.User](ctx, "currentUser")
+	var journals []*models.Journal
+
+	c.db.
+		Where("creator_id = ?", currentUser.ID).
+		Where("date >= ?", time.Now().AddDate(0, -1, 0)).
+		Preload("Rating").
+		Preload("JournalType").
+		Order("created_at desc").
+		Find(&journals)
+
+	ctx.Locals("journals", &journals)
+
+	return ctx.Next()
+}
+
 func (c *DashboardController) RegisterViewRoutes() {
 	c.views.Use(middleware.RequireAuth)
-	c.views.Get("/", middleware.SetRatings, middleware.SetJournalTypes, utils.RenderPage(dashboard.DashboardPage))
+	c.views.Get("/", middleware.SetRatings, middleware.SetJournalTypes, c.getJournals, utils.RenderPage(dashboard.DashboardPage))
 }
 
 func (c *DashboardController) RegisterApiRoutes() {
